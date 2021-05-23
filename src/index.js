6e56f428b13d9aea752eae0620db0827c2536fae
b00kbaker <caimmurphy@gmail.com>
2021-05-23 12:09:05 -0600

    Fixed header/nav deafult

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { HashRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import render from 'react-dom'
+import HashRouter from 'react-router-dom'
 import App from './App';
 
 render((
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Navbar } from "../components/Navbar";
-import { Project } from "../components/Project";
+import Navbar from "../components/Navbar";
+import Project from "../components/Project";
 
 const Portfolio = () => {
     return (
