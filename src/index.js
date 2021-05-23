0c19f7a882182c656108ab5c4dc27d4d7001d23e
b00kbaker <caimmurphy@gmail.com>
2021-05-23 11:48:46 -0600

    Double checking

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter } from 'react-router-dom'
-import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 
 render((
   <HashRouter>
